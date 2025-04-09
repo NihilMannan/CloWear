f05b8d770022880a130dc3921072d91bd70a314a
NihilMannan <nihilmannan@gmail.com>
2025-04-09 14:25:42 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ backgroundColor: '#000', color: '#fff', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Welcome to CloWear</h1>
-    </div>
-  );
-}
