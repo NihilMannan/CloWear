ed4f02a4256a1727d1f21c5d5d495d79b8fb020c
NihilMannan <nihilmannan@gmail.com>
2025-04-09 14:26:57 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#000', color: '#fff', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Welcome to CloWear</h1>
+    </div>
+  );
+}
